b0befa343d92af8b60d4dcce2138ab7f6cfd4917
OlegGribOle <oleggribpoker@gmail.com>
2024-12-24 18:52:17 +0300

    Первый коммит

--- src/Task.java
+++ src/Task.java
@@ -1,12 +1,15 @@
 public class Task {
 
-    public static void printThreeWords(){
+    public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
-    public static void checkSumSign(int a, int b){
-
+    public static void checkSumSign(int a, int b) {
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else
+            System.out.println("Сумма отрицательнная");
     }
 }
