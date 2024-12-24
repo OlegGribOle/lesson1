108b6f9c4d73b9291f61048d5a8b2e0126c62dd9
OlegGribOle <oleggribpoker@gmail.com>
2024-12-24 18:29:29 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public void printThreeWords2(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
