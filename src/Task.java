9377a17aba785a839c72fdc5a788757d62466931
OlegGribOle <oleggribpoker@gmail.com>
2024-12-26 18:29:24 +0300

    Готово. Добавлены номера заданий при выводе в косоль

--- src/Task.java
+++ src/Task.java
@@ -1,12 +1,15 @@
 public class Task {
 
     public static void printThreeWords() {
+        System.out.println("Задание 1:");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
     public static void checkSumSign(int a, int b) {
+        System.out.println();
+        System.out.println("Задание 2:");
         if (a + b >= 0) {
             System.out.println("Сумма положительная");
         } else
@@ -14,6 +17,8 @@ public class Task {
     }
 
     public static void printColor(int value) {
+        System.out.println();
+        System.out.println("Задание 3:");
         if (value <= 0) {
             System.out.println("Красный");
         } else if (0 < value && value <= 100) {
@@ -23,6 +28,8 @@ public class Task {
     }
 
     public static void compareNumbers() {
+        System.out.println();
+        System.out.println("Задание 4:");
         int a = 5;
         int b = 3;
         if (a >= b)
@@ -33,25 +40,35 @@ public class Task {
     }
 
     public static boolean betweenNumbers(int a, int b) {
+        System.out.println();
+        System.out.println("Задание 5:");
         return a + b >= 10 && a + b <= 20;
     }
 
     public static void numMoreLess(int a) {
+        System.out.println();
+        System.out.println("Задание 6:");
         if (a < 0)
             System.out.println("Число отрицательное");
         else System.out.println("Число положительное");
     }
 
     public static boolean boolNumMoreLess(int a) {
+        System.out.println();
+        System.out.println("Задание 7:");
         return a < 0;
     }
 
     public static void copyString(String str, int copy) {
+        System.out.println();
+        System.out.println("Задание 8:");
         for (int i = 0; i < copy; i++)
             System.out.println(str);
     }
 
     public static boolean isLeapYear() {
+        System.out.println();
+        System.out.println("Задание 9:");
         int year = 2000;
         if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
             return true;
@@ -59,6 +76,8 @@ public class Task {
     }
 
     public static void rotateZeroOne() {
+        System.out.println();
+        System.out.println("Задание 10:");
         int[] arr = {1, 1, 0, 1, 0, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -73,6 +92,8 @@ public class Task {
     }
 
     public static void writeInArray() {
+        System.out.println();
+        System.out.println("Задание 11:");
         int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
@@ -83,6 +104,8 @@ public class Task {
     }
 
     public static void doubleLessSix() {
+        System.out.println();
+        System.out.println("Задание 12:");
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
@@ -94,8 +117,9 @@ public class Task {
 
     }
 
-    public static void xSqadArray() {
-        int[][] arr1 = new int[5][5];
+    public static void xSqadArray() {System.out.println();
+        System.out.println("Задание 13:");
+        int[][] arr1 = new int[4][4];
         for (int i = 0; i < arr1.length; i++) {
             int[] arr2 = arr1[i];
             for (int j = 0; j < arr2.length; j++) {
@@ -111,6 +135,8 @@ public class Task {
     }
 
     public static void createArray(int len, int initialValue) {
+        System.out.println();
+        System.out.println("Задание 14:");
         int[] arr = new int[len];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = initialValue;
